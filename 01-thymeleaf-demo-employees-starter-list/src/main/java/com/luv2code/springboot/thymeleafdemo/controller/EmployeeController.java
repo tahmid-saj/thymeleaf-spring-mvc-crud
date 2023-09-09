3bf54b95af03bf5c6941b6bdef4223f6f61c74ed
tahmid-saj <tahmid.saj@gmail.com>
2023-09-09 16:47:36 -0400

    adding employee

--- 01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,6 +37,19 @@ public class EmployeeController {
 		Employee theEmployee = new Employee();
 
 		theModel.addAttribute("employee", theEmployee);
+
+		return "employees/employee-form";
+	}
+
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+		// get the employee from the service
+		Employee theEmployee = employeeService.findById(theId);
+
+		// set employee in the model to prepopulate the form
+		theModel.addAttribute("employee", theEmployee);
+
+		// send over to our form
 
 		return "employees/employee-form";
 	}
