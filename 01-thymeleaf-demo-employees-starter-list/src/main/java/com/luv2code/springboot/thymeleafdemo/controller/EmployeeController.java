073261f8f6afc583d559b31ff6a1203d96292f64
tahmid-saj <tahmid.saj@gmail.com>
2023-09-09 16:04:32 -0400

    adding employee

--- 01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -40,6 +42,16 @@ public class EmployeeController {
 		theModel.addAttribute("employee", theEmployee);
 
 		return "employees/employee-form";
+	}
+
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+		// save the employee
+		employeeService.save(theEmployee);
+
+		// use a redirect to prevent duplicate submissions
+		return "redirect:/employees/list";
+
 	}
 }
 
